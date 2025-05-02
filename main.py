e5be7290a48cd9fbfa761bee3970352e1b27aaea
NotAKes <anton_fcsm920@mail.ru>
2025-05-02 23:00:24 +0300

    user blueprint

--- main.py
+++ main.py
@@ -7,16 +7,28 @@ from forms.create_event import CreateEvent
 from data.users import User
 from data import db_session
 from forms.login import LoginForm
-
+import flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 upload_folder = os.path.join('static', 'uploads')
 app.config['UPLOAD'] = upload_folder
 
+user_blueprint = flask.Blueprint(
+    'user_blueprint',
+    __name__,
+    template_folder='templates'
+)
+
+api_blueprint = flask.Blueprint(
+    'api_blueprint',
+    __name__,
+    template_folder='templates'
+)
 
 def main():
     db_session.global_init("db/database.db")
+    app.register_blueprint(user_blueprint)
     app.run()
 
 
@@ -30,7 +42,7 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.route("/")
+@user_blueprint.route("/")
 def index():
     db_sess = db_session.create_session()
     events = sorted(db_sess.query(Event).all(), key=lambda a: a.start_date)
@@ -38,7 +50,7 @@ def index():
     return render_template("index.html", events=events)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@user_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -61,7 +73,7 @@ def register():
     return render_template('register.html', title='Регистрация', form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@user_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -76,7 +88,7 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
-@app.route('/logout')
+@user_blueprint.route('/logout')
 @login_required
 def logout():
     logout_user()
@@ -84,7 +96,7 @@ def logout():
 
 
 
-@app.route('/event/create', methods=['GET', 'POST'])
+@user_blueprint.route('/event/create', methods=['GET', 'POST'])
 @login_required
 def create_event():
     got_id = current_user.get_id()
@@ -109,11 +121,17 @@ def create_event():
         return redirect('/')
     return render_template('create_event.html', title='Создание мероприятия', form=form)
 
-@app.route('/profile')
+@user_blueprint.route('/profile')
 @login_required
 def profile():
     return render_template('create_event.html', title='Ваш профиль', form=[])
+    # TODO
 
+
+@user_blueprint.route('/event/<int:id>', methods=['GET', 'POST'])
+@login_required
+def get_event_id():
+    return render_template('index.html', title='id', form=[])
 
 if __name__ == '__main__':
     main()
